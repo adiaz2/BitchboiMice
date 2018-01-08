7049626a20c497da1ba959d98e3d7a5b8c476439
arios215 <35086042+arios215@users.noreply.github.com>
2018-01-08 12:39:14 -0700

    Add files via upload

---
+++ Encoder_Reader_and_Turning_Functions.ino
@@ -0,0 +1,169 @@
+  int D1=2;     //H Bridge Outputs
+  int D2=3;
+  int D3=6;
+  int D4=7;
+
+  //Encoders
+  int LE;       //Left Encoder
+  int RE;       //Right Encode
+  int LESum=0;  //Left Encoder Sum
+  int RESum=0;  //Right Encoder Sum
+  int LI;       //Left Instuction
+  int RI;       //Right Instructions
+
+  //IR
+  int IR1;      //IR 1
+  int IR2;      //IR 2
+  int IR3;      //IR 3
+  int IR4;      //IR 4
+  int IR5;      //IR 5
+  int TH=1000;  //Threshold
+
+void setup() {
+  
+  //Moter
+  pinMode(D1, OUTPUT);  //Delcaring Outputs
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+
+  //IR
+  IR1= analogRead(2);   //Setting IRs to connect to Analog Pin
+  IR2= analogRead(3);
+  IR3= analogRead(4);
+  IR4= analogRead(5);
+  IR5= analogRead(6);
+
+
+  LI=100;
+  RI=100;
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  LE= digitalRead(5);
+  RE= digitalRead(9);
+  LESum+=LE;
+  RESum+=RE;
+  if(LESum<=LI && RESum<=RI)
+  {
+    RT();
+  }
+  else
+  {
+    STP();
+  }
+}
+
+//Functions
+//====================================================
+//Turning Fucntions
+void LT() //Left Turn
+{
+  LCCW();
+  RCW();
+}
+void RT() //Right Turn
+{
+  RCCW();
+  LCW();
+}
+void FW() //Foward
+{
+  LCW();
+  RCW();
+}
+void BW() //Backwards
+{
+  LCCW();
+  RCCW();
+}
+void STP() //Stop
+{
+  LSTP();
+  RSTP();
+}
+
+//Wheel Functions
+
+//Left Functions 
+void LCW()               //Left Clock Wise
+{
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, HIGH);
+}
+void LCCW()              //Left Counter Clock Wise
+{
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, LOW);
+}
+void LSTP()              //Left Stop
+{
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+}
+
+
+//Right Functions
+void RCW()              //Right Clock Wise
+{
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+}
+void RCCW()             //Right Counter Clock Wise
+{
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, HIGH);
+}
+void RSTP()             //Right Stop
+{
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW);
+}
+
+//IR Functions
+int IRC(int x)        //Function to Check if Somthing is Close to IR
+{
+  if (x==1)
+  {
+    IR1= analogRead(2);
+    if (IR1<TH)
+      return 1;
+    else
+      return 0;
+  }
+  if (x==2)
+  {
+    IR2= analogRead(3);
+    if (IR2<TH)
+      return 1;
+    else
+      return 0;
+  }
+  if (x==3)
+  {
+    IR3= analogRead(4);
+    if (IR3<TH)
+      return 1;
+    else
+      return 0;
+  }
+  if (x==4)
+  {
+    IR4= analogRead(5);
+    if (IR4<TH)
+      return 1;
+     else
+      return 0;
+  }
+  if (x==5)
+  {
+    IR5= analogRead(6);
+    if (IR5<TH)
+      return 1;
+    else
+      return 0;
+  }
+}
+

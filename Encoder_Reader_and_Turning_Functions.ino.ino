18cd3e00a61780526dab8af81a455e03730b52c7
arios215 <35086042+arios215@users.noreply.github.com>
2018-02-08 19:53:53 -0700

    Add files via upload

--- Encoder_Reader_and_Turning_Functions.ino.ino
+++ Encoder_Reader_and_Turning_Functions.ino.ino
@@ -1,5 +1,35 @@
-
+#include <Wire.h>
+#include <SPI.h>
+#include <math.h>
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h>  // not used in this demo but required!
 #include <digitalWriteFast.h>
+
+  // i2c
+  Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+  int startDegree = 0;
+  int sensitivity = 10;
+  int degree;
+  int north;
+  int northHigh;
+  int northLow;
+  int east;
+  int eastHigh;
+  int eastLow;
+  int south;
+  int southHigh;
+  int southLow;
+  int west;
+  int westHigh;
+  int westLow;
+  bool firstTime = true;
+
+  #define LSM9DS1_SCK A5
+  #define LSM9DS1_MISO 12
+  #define LSM9DS1_MOSI A4
+  #define LSM9DS1_XGCS 6
+  #define LSM9DS1_MCS 5
+
   
   int D1=6;     //H Bridge Outputs
   int D2=7;
@@ -35,17 +65,35 @@
 
   boolean movingForward = false;
 
+void setupSensor()
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
+  
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
 
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
+}
 
 void setup() {
   attachInterrupt(digitalPinToInterrupt(LEA), isl, RISING);  
   attachInterrupt(digitalPinToInterrupt(REA), isr, RISING);
   
   //Moter
-  pinMode(D1, OUTPUT);  //Delcaring Outputs
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
+  pinMode(A1, OUTPUT);  //Delcaring Outputs
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A6, OUTPUT);
   pinMode(LEnable, OUTPUT);
   pinMode(REnable, OUTPUT);
 
@@ -55,6 +103,25 @@ void setup() {
   IR3= analogRead(4);
   IR4= analogRead(5);
   IR5= analogRead(6);
+
+    while (!Serial) {
+    delay(1); // will pause Zero, Leonardo, etc until serial console opens
+  }
+  
+  Serial.println("LSM9DS1 data read demo");
+  
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
+    while (1);
+  }
+  Serial.println("Found LSM9DS1 9DOF");
+
+  // helper to just set the default scaling we want, see above!
+  setupSensor();
+
+  //startDegree = atan(m.magnetic.y/m.magnetic.x)*180/M_PI);
   
   Serial.begin(9600);
 }
@@ -63,9 +130,82 @@ int x;
 
 void loop() 
 {
+    lsm.read();  /* ask it to read in the data */ 
+  
+    /* Get a new sensor event */ 
+    sensors_event_t a, m, g, temp;
+  
+    lsm.getEvent(&a, &m, &g, &temp); 
+
     analogWrite(REnable, masterPower);
     analogWrite(LEnable, slavePower);   //Speed
     FW();
+
+    Serial.println();
+  degree = atan(m.magnetic.y/m.magnetic.x)*360/M_PI;
+  
+  if(degree < 0){
+    degree += 360;
+  }
+  if(degree>360){
+    degree -= 360;
+  }
+  if(firstTime){
+    startDegree = degree;
+    
+    north = startDegree;
+    if(north<sensitivity){
+      northLow = 360-sensitivity+north;
+    }
+    else{
+      northLow = (north-sensitivity)%360;
+    }
+    northHigh = (north+sensitivity)%360;
+    
+    east = (startDegree+90)%360;
+    if(east<sensitivity){
+      eastLow = 360-sensitivity+east;
+    }
+    else{
+      eastLow = (east-sensitivity)%360;
+    }
+    eastHigh = (east+sensitivity)%360;
+    
+    south = (startDegree+180)%360;
+    if(south<sensitivity){
+      southLow = 360-sensitivity+south;
+    }
+    else{
+      southLow = (south-sensitivity)%360;
+    }
+    southHigh = (south+sensitivity)%360;
+    
+    west = (startDegree+270)%360;
+    if(west<sensitivity){
+      westLow = 360-sensitivity+west;
+    }
+    else{
+      westLow = (west-sensitivity)%360;
+    }
+    westHigh = (west+sensitivity)%360;
+    
+    firstTime = false;
+  }
+  if(degree>northLow && degree<northHigh){
+    Serial.println("North");
+  }
+  else if(degree>eastLow && degree<eastHigh){
+    Serial.println("East");
+  }
+  else if(degree>southLow && degree<southHigh){
+    Serial.println("South");
+  }
+  else if(degree>westLow && degree<westHigh){
+    Serial.println("West");
+  }
+  
+  Serial.println(degree);
+  Serial.println();
     
    
     
@@ -85,7 +225,7 @@ void loop()
 void isl()                                        //Left Encoder
 {
       VPL++;
-  Serial.print("Left");
+//  Serial.print("Left");
   //Serial.print(VPL);
  // Serial.print("\n");
  }
@@ -93,7 +233,7 @@ void isl()                                        //Left Encoder
 void isr()                                        //Right Encoder
 {
   VPR++;
-  Serial.print("Right");
+  //Serial.print("Right");
   //Serial.print(VPR);
   //Serial.print("\n");
  }
@@ -107,8 +247,12 @@ void LT() //Left Turn
 }
 void RT() //Right Turn
 {
-  RCCW();
-  LCW();
+  int currentDegree = degree;
+  while(degree){
+      RCCW();
+      LCW(); 
+  }
+
 }
 void FW() //Foward
 {
